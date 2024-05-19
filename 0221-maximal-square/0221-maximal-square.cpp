d1e4af7565830441dd0eb4c6d3db895b1a5a9c36
Peacefull-beast <73907856+Peacefull-beast@users.noreply.github.com>
2024-05-19 15:30:27 +0530

    Time: 62 ms (58.06%), Space: 22.3 MB (89.11%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -5,8 +5,25 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         
-        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+//         vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
         
+//         int ans = 0;
+//         for(int i = 1; i<=n; i++)
+//         {
+//             for(int j = 1; j<=m; j++)
+//             {
+//                 if(matrix[i-1][j-1] == '1')
+//                 {
+//                      dp[i][j] = min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1])) + 1;
+//                      ans = max(ans, dp[i][j]);
+//                 }   
+                
+//             }
+//         }
+//         return ans*ans;
+        
+        vector<int>prevdp(m+1, 0);
+        vector<int>dp(m+1, 0);
         int ans = 0;
         for(int i = 1; i<=n; i++)
         {
@@ -14,12 +31,16 @@ public:
             {
                 if(matrix[i-1][j-1] == '1')
                 {
-                     dp[i][j] = min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1])) + 1;
-                     ans = max(ans, dp[i][j]);
-                }   
+                     dp[j] = min(prevdp[j], min(dp[j-1], prevdp[j-1])) + 1;
+                     ans = max(ans, dp[j]);
+                }
+                else
+                    dp[j] = 0;
                 
             }
+            prevdp = dp;
         }
         return ans*ans;
     }
+    
 };
