d4e64b60a58da669c00b6300d9d901c79b7b33c8
Chandan Singh <73907856+Peacefull-beast@users.noreply.github.com>
2024-06-25 12:20:58 +0530

    Time: 2 ms (49.63%), Space: 10.2 MB (12.64%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -11,59 +11,15 @@
  */
 class Solution {
 public:
-    int bsearch(int target, vector<int>&o)
-    {
-        int low = 0;
-        int high = o.size()-1;
-        while(low <= high)
-        {
-            int mid = (low+high)/2;
-            if(target == o[mid])
-                return mid;
-            if(target > o[mid])
-                low = mid+1;
-            else
-                high = mid-1;
-        }
+    int s= 0;
+    TreeNode* bstToGst(TreeNode* root) {
         
-        return -1;
-        
-    }
-    void solve(TreeNode* root, vector<int>& order, vector<int>& psum)
-    {
-        if(root == NULL)
-            return;
-        int ind = bsearch(root->val, order);
-        root->val = psum[ind];
-        solve(root->left,order,psum);
-        solve(root->right,order,psum);
-    }
-    void ino(TreeNode* root, vector<int>& order)
-    {
-        if(root == NULL)
-            return;
-        ino(root->left, order);
-        order.push_back(root->val);
-        ino(root->right, order);
-        
-    }
-    TreeNode* bstToGst(TreeNode* root) 
-    {
-        vector<int>order;
-        ino(root, order);
-        
-        int sum = 0;
-        int n=order.size();
-        vector<int>psum(n, 0);
-        
-        for(int i=n-1; i>=0; i--)
-        {
-            sum+=order[i];
-            psum[i]=sum;
-        }
-        solve(root,order,psum);
-        
-        return root;
+        if(root->right) bstToGst(root->right);
 
+        s+=root->val;
+        root->val=s;
+
+        if(root->left) bstToGst(root->left);
+        return root;
     }
 };
