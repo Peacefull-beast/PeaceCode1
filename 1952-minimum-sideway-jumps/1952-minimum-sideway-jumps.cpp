cb7329d7d7292d50a74f8fed5e4c8d2e5a32950f
Chandan Singh <73907856+Peacefull-beast@users.noreply.github.com>
2024-09-03 18:33:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1952-minimum-sideway-jumps/1952-minimum-sideway-jumps.cpp
+++ 1952-minimum-sideway-jumps/1952-minimum-sideway-jumps.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
     int n;
-    
     int solve(vector<int>& obstacles, int ind, int lane, vector<vector<int>>& dp) {
-       
-        if (ind == n - 1)
+        if (ind == obstacles.size() - 1) 
             return 0;
 
-        
-        if (dp[ind][lane] != -1)
-            return dp[ind][lane];
-
-        
         if (obstacles[ind] == lane)
             return dp[ind][lane] = n + 1;
 
         
-        int minJumps = INT_MAX;
 
-        for (int i = 1; i <= 3; ++i) {
-            if (i != lane && obstacles[ind] != i) 
-                minJumps = min(minJumps, 1 + solve(obstacles, ind + 1, i, dp));
+        
+        if (dp[ind][lane] != -1) 
+            return dp[ind][lane];
 
-            else 
-                minJumps = min(minJumps, solve(obstacles, ind + 1, lane, dp));
-        }
+        if(obstacles[ind+1]==0)return solve(obstacles, ind + 1, 1, dp);
 
-        return dp[ind][lane] = minJumps;
+        int o1 = (obstacles[ind + 1] != 1) ? ((lane == 1 ? 0 : 1) + solve(obstacles, ind + 1, 1, dp)) : n + 1;
+        int o2 = (obstacles[ind + 1] != 2) ? ((lane == 2 ? 0 : 1) + solve(obstacles, ind + 1, 2, dp)) : n + 1;
+        int o3 = (obstacles[ind + 1] != 3) ? ((lane == 3 ? 0 : 1) + solve(obstacles, ind + 1, 3, dp)) : n + 1;
+
+
+        return dp[ind][lane] = min({o1, o2, o3});
     }
-    
+
     int minSideJumps(vector<int>& obstacles) {
         n = obstacles.size();
         vector<vector<int>> dp(n, vector<int>(4, -1)); 
 
+       
         return solve(obstacles, 0, 2, dp);
     }
 };
