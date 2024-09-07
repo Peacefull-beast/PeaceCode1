1cfa2b90dd488410b60049e29f41b9e14f8f8e25
Chandan Singh <73907856+Peacefull-beast@users.noreply.github.com>
2024-09-07 12:09:32 +0530

    Time: 27 ms (41.65%), Space: 30.9 MB (36.5%) - LeetHub

---
+++ 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool dfs(ListNode* head, TreeNode* root) {
+        if (head == nullptr) return true; 
+        if (root == nullptr) return false;
+
+        
+        if (head->val == root->val)
+            return dfs(head->next, root->left) || dfs(head->next, root->right);
+        
+        return false;  
+    }
+    bool isSubPath(ListNode* head, TreeNode* root) 
+    {
+        if (root == nullptr) return false;
+        bool o1 = dfs(head, root); 
+        bool o2 = isSubPath(head, root->left);
+        bool o3 = isSubPath(head, root->right);
+
+        return o1 || o2 || o3;
+
+        
+    }
+};
