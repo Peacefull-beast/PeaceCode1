ce2b12575f7c611b7e9833ca6f8c1b404cf568f2
Chandan Singh <73907856+Peacefull-beast@users.noreply.github.com>
2024-10-19 13:10:30 +0530

    Time: 0 ms (100%), Space: 7.5 MB (64.16%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -8,16 +8,16 @@ public:
 
         int len = (1 << n) - 1;
 
-        int mid = len/2 + 1;
+        int mid = len/2 + 1;//find length of find string
 
         if(k == mid)
-        return  '1';
+        return  '1';//that's how string is derived
 
         else if(k < mid)
-        return findKthBit(n-1, k);
+        return findKthBit(n-1, k);//if k is in first half it lies in sn-1
 
         else
-        return findKthBit(n-1, len-k+1) == '0' ? '1' : '0';
+        return findKthBit(n-1, len-k+1) == '0' ? '1' : '0';//if lies in second half invert bit in 1st half of k
 
 
     }
